7927e041f88d68f6e87fb9b054a9df769c243c89
Kayky Dion <kayky.dion@gmail.com>
2024-07-04 19:25:32 -0300

    Parametrizando cards de tarefa

--- src/components/Tarefa/index.tsx
+++ src/components/Tarefa/index.tsx
@@ -14,19 +14,21 @@ const Tarefa = ({ descricao, prioridade, status, titulo }: Props) => {
   return (
     <S.Card>
       <S.Titulo>{titulo}</S.Titulo>
-      <S.Tag>{prioridade}</S.Tag>
-      <S.Tag>{status}</S.Tag>
+      <S.Tag prioridade={prioridade}>{prioridade}</S.Tag>
+      <S.Tag status={status}>{status}</S.Tag>
       <S.Descricao value={descricao} />
       <S.BarraAcoes>
         {estaEditando ? (
           <>
-            <S.Botao>Salvar</S.Botao>
-            <S.Botao onClick={() => setEstaEditando(false)}>Cancelar</S.Botao>
+            <S.BotaoSalvar>Salvar</S.BotaoSalvar>
+            <S.BotaoCancelarERemover onClick={() => setEstaEditando(false)}>
+              Cancelar
+            </S.BotaoCancelarERemover>
           </>
         ) : (
           <>
             <S.Botao onClick={() => setEstaEditando(true)}>Editar</S.Botao>
-            <S.Botao>Remover</S.Botao>
+            <S.BotaoCancelarERemover>Remover</S.BotaoCancelarERemover>
           </>
         )}
       </S.BarraAcoes>
