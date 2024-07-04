a088e118d6f627e6cd11bbf614e4bfa548179f76
Kayky Dion <kayky.dion@gmail.com>
2024-07-04 19:46:12 -0300

    Adicionando enums como parâmetros das tags

---
+++ src/utils/enums/Tarefa.ts
@@ -0,0 +1,11 @@
+export enum Prioridade {
+  URGENTE = 'urgente',
+  IMPORTANTE = 'importante',
+  NORMAL = 'normal'
+}
+
+export enum Status {
+  PENDENTE = 'pendente',
+  CONCLUIDO = 'concluída',
+  NORMAL = 'normal'
+}
